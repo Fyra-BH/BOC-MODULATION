966e3c506cf57906a930fc5f44b2053a03fa64b8
Fyra-BH <2472708282@qq.com>
2019-12-13 01:45:44 +0800

    编译脚本

---
+++ code/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+javac -d ./ FBComplexList.java
+javac -d ./ FBTools.java 
+javac -d ./ FBComplex.java
+javac -d ./ FBChart.java 
+javac -d ./ FBDataGen.java 
+javac -d ./ FBMainForm.java 
+javac -d ./ FBConsole.java 
