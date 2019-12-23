fa2a9f79c955b3ccb6ddca474370d8c04f04d42a
Fyra-BH <2472708282@qq.com>
2019-12-24 00:24:34 +0800

    时域

--- code/FBBocCal.java
+++ code/FBBocCal.java
@@ -5,11 +5,13 @@
 package fbcode.math;
 
 import fbcode.tools.FBConsole;
+import fbcode.gui.FBChartFrame;
 import fbcode.math.FBDataGen;
+import fbcode.math.FBDsp;
 
 
 /**
- * 专门用于BOC参数计算的类
+ * 专门用于BOC计算的类
  */
 public class FBBocCal{
 
@@ -22,7 +24,7 @@ public class FBBocCal{
      * @param a     BOC_ALPHA
      * @param b     BOC_BETA
      * @param bw    带宽
-     * @return
+     * @return      功率普密度
      */
     public static double[] getGBOC(int a,int b,Double bw){
 
@@ -54,5 +56,42 @@ public class FBBocCal{
 
     }
 
+    /**
+     * 获取随机基带信号
+     * @param n     一个码元时间内亚载波半周期数
+     * @param fs    亚载波速率
+     * @param t     离散的时间(类似matlab中的用法)
+     * @return      时域基带信号
+     */
+    public static double[]  getBBS(int n,double fs, double[] t){
+        double[] res=new double[t.length];
+        double ts=1/fs;//亚载波半周期
+        double dur=t[t.length-1]-t[0];//时长
+        int[] an=FBDsp.getRandomCode((int)(dur/n/ts+1), 0.5);//获取随机扩频码
+        for (int i = 0; i < res.length; i++) {
+            res[i]=an[(int)(i*an.length/res.length)];
+        }
+        return res;
+    }
+
+    /**
+     * 获取随机基带信号
+     * @param alpha     boc参数
+     * @param beta      同上
+     * @param t         离散的时间(类似matlab中的用法)
+     * @return
+     */
+    public static double[] getBBS(int alpha,int beta,double[] t ){
+
+        return getBBS((int)alpha*2/beta , (double)alpha*1.023e6,  t);
+    }
+
+
+    public static void main(String[] args) {
+        double[] t=FBDataGen.getLineSeq(0, 1/1.023e6*20, 1000);
+        double[] s= FBBocCal.getBBS(10, 5, t);
+        new FBChartFrame(t, s);
+       
+    }
     
 }
